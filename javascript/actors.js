1a7e06f60e7a264d7e6820857700cdfdb30ca625
Andrew Gardner <timebot@gmail.com>
2012-11-04 01:04:36 -0500

    Animation and Spritesheet classes added - character given static

--- javascript/actors.js
+++ javascript/actors.js
@@ -1,9 +1,9 @@
 var gamejs = require('gamejs');
 var objects = require('gamejs/utils/objects');
 
-var Unit = exports.Unit = function(pos, image, isPlayer) {
+var Unit = exports.Unit = function(pos, spriteSheet, isPlayer) {
     Unit.superConstructor.apply(this, arguments);
-
+		
     this.speed = 200;
 
     this.gravity = 150;
@@ -11,13 +11,12 @@ var Unit = exports.Unit = function(pos, image, isPlayer) {
 
     this.angle = null;
 
-    this.origImage = image;
-    var imgSize = this.origImage.getSize();
-
-    this.image = new gamejs.Surface(imgSize);
-    this.image.blit(this.origImage);
-
-    this.rect = new gamejs.Rect(pos, imgSize);
+    this.origImage = spriteSheet.get(0);
+	this.animation = new Animation(spriteSheet,
+		{'static': [0,2]}, 6);
+	this.animation.start('static');
+		
+    this.rect = new gamejs.Rect([0, 0], [27,19]);
 
     this.canJump = false;
     this.onGround;
@@ -30,6 +29,8 @@ var Unit = exports.Unit = function(pos, image, isPlayer) {
 objects.extend(Unit, gamejs.sprite.Sprite);
 
 Unit.prototype.update = function(msDuration, map) {
+	this.animation.update(msDuration);
+	this.image = this.animation.image;
     // Check that this sprites group is not colliding with the map
     var colliding = false;
     gamejs.sprite.groupCollide(this.groups[0], map.collisionable).forEach(function(collision) {
@@ -85,3 +86,75 @@ Unit.prototype.update = function(msDuration, map) {
         )
 };
 
+var SpriteSheet = exports.SpriteSheet = function(imagePath, sheetSpec) {
+   this.get = function(id) {
+      return surfaceCache[id];
+   };
+
+   var width = sheetSpec.width;
+   var height = sheetSpec.height;
+   var image = gamejs.image.load(imagePath);
+   var surfaceCache = [];
+   var imgSize = new gamejs.Rect([0,0],[width,height]);
+   // extract the single images from big spritesheet image
+   for (var i=0; i<image.rect.width; i+=width) {
+       for (var j=0;j<image.rect.height;j+=height) {
+         var surface = new gamejs.Surface([width, height]);
+         var rect = new gamejs.Rect(i, j, width, height);
+         surface.blit(image, imgSize, rect);
+         surfaceCache.push(surface);
+      }
+   }
+   return this;
+};
+
+var Animation = function(spriteSheet, animationSpec, fps) {
+	this.fps = fps || 6;
+	this.frameDuration = 1000 / this.fps;
+	this.spec = animationSpec;
+	
+	this.currentFrame = null;
+	this.currentFrameDuration = 0;
+	this.currentAnimation = null;
+	
+	this.spriteSheet = spriteSheet;
+	
+	this.loopFinished = false;
+	
+	this.image = spriteSheet.get(0);
+	return this;
+};
+
+Animation.prototype.start = function(animation) {
+	this.currentAnimation = animation;
+	this.currentFrame = this.spec[animation][0];
+	this.currentFrameDuration = 0;
+	this.update(0);
+	return;
+};
+
+Animation.prototype.update = function(msDuration) {
+	if (!this.currentAnimation) {
+		throw new Error('No animation started.');
+	}
+	
+	this.currentFrameDuration += msDuration;
+	if (this.currentFrameDuration >= this.frameDuration){
+		this.currentFrame++;
+		this.currentFrameDuration = 0;
+	
+		var aniSpec = this.spec[this.currentAnimation];
+		if (aniSpec.length == 1 || this.currentFrame > aniSpec[1]) {
+			this.loopFinished = true;
+			
+			if (aniSpec.length === 3 && aniSpec[2] === false) {
+				this.currentFrame--;
+			} else {
+				this.currentFrame = aniSpec[0];
+			}
+		}
+	}
+	
+	this.image = this.spriteSheet.get(this.currentFrame);
+	return;
+};
--- javascript/main.js
+++ javascript/main.js
@@ -2,36 +2,43 @@ var gamejs = require('gamejs');
 var view = require('./view');
 var input = require('./input');
 var Unit = require('./actors').Unit;
+var SpriteSheet = require('./actors').SpriteSheet;
 
 var main = function() {
     gamejs.display.setCaption('Game Off')
     var display = gamejs.display.setMode([800, 600]);
-
+	
     var map = new view.Map('./data/grassland.tmx');
 
     var units = new gamejs.sprite.Group();
-
+	console.debug('easy');
     // Spawn a character, normally our main player, but what if we 
     // want to introduce baddies?!
-    var spawn = function(image, isPlayer, yPos) {
+    var spawn = function(spriteSheet, isPlayer, yPos) {
         var pos = [0, 0];
         
         var unit = new Unit(
             pos,
-            image,
+            spriteSheet,
             true // isPlayer
         );
         units.add(unit);
         return unit;
     };
-
+	console.debug('check');
+	
+	var filename = 'images/meatboy.png';
+	var dimensions = {width:28, height:19};
+	console.debug('gonna make a spritesheet');
+	var spriteSheet = new SpriteSheet(filename, dimensions);
+	console.debug('about to make a guy');
     // meat!
     var player = spawn(
-        gamejs.image.load('images/meatboy.png'),
+        spriteSheet,
         true,
         null
     );
-
+	console.debug('made a guy');
     var gameController = new input.GameController(player);
 
     // The game loop
@@ -46,7 +53,7 @@ var main = function() {
         // Draw
         map.draw(display);
         units.draw(display);
-
+		
         player.angle = gameController.angle();
         player.jumped = gameController.jumped();
     };
